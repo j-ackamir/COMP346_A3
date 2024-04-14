556417a360bcb41a9b891913f9ebf2a8a8da6349
j-ackamir <133796209+j-ackamir@users.noreply.github.com>
2024-04-14 12:22:17 -0400

    Add files via upload

---
+++ Monitor.java
@@ -0,0 +1,101 @@
+import java.util.concurrent.locks.Condition;
+
+/**
+ * Class Monitor
+ * To synchronize dining philosophers.
+ *
+ * @author Serguei A. Mokhov, mokhov@cs.concordia.ca
+ */
+public class Monitor
+{
+	/*
+	 * ------------
+	 * Data members
+	 * ------------
+	 */
+	enum State {THINKING, EATING, HUNGRY};
+	private State[] states;
+	private Object[] forks;
+
+
+	/**
+	 * Constructor
+	 */
+	public Monitor(int piNumberOfPhilosophers)
+	{
+		// TODO: set appropriate number of chopsticks based on the # of philosophers
+
+		states= new State[piNumberOfPhilosophers];
+		forks=new Object[piNumberOfPhilosophers];
+
+		for(int i=0; i<piNumberOfPhilosophers; i++)
+		{
+			states[i]=State.THINKING;
+			forks[i]=new Object();
+			
+		}
+		
+	}
+
+	/*
+	 * -------------------------------
+	 * User-defined monitor procedures
+	 * -------------------------------
+	 */
+
+	/**
+	 * Grants request (returns) to eat when both chopsticks/forks are available.
+	 * Else forces the philosopher to wait()
+	 */
+	public synchronized void pickUp(final int piTID)
+	{
+		// ...
+		states[piTID-1]=State.HUNGRY;
+		test(piTID);
+		if (states[piTID]!=State.EATING)
+		{
+			self[piTID].wait();
+		}
+	}
+
+	/**
+	 * When a given philosopher's done eating, they put the chopstiks/forks down
+	 * and let others know they are available.
+	 */
+	public synchronized void putDown(final int piTID)
+	{
+		// ...
+		states[piTID-1]=State.THINKING;
+		test((piTID+1) % states.length);
+		test((piTID+(states.length-1)) % states.length);
+		
+	}
+	public synchronized void test (final int i)
+	{
+		if (states[(i+1) % states.length] != State.EATING && states[i] == State.HUNGRY && states[(i+(states.length-1)) % states.length] != State.EATING)
+		{ 
+			states[i]=State.EATING;
+			self[i].signal();
+		}
+	}
+
+	/**
+	 * Only one philopher at a time is allowed to philosophy
+	 * (while she is not eating).
+	 */
+	public synchronized void requestTalk()
+	{
+		// ...
+	}
+
+	/**
+	 * When one philosopher is done talking stuff, others
+	 * can feel free to start talking.
+	 */
+	public synchronized void endTalk()
+	{
+		// ...
+	}
+}
+
+// EOF
